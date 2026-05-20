9326956953961aa4014f277ffbe2f7d6ba8b2810
Ibrahima DIOP <rahimpresidiop@gmail.com>
2026-05-20 08:42:09 +0000

    feat: installer le pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,10 +59,29 @@ export default function RootLayout({
   return (
     <html lang="fr" className="scroll-smooth">
       <head>
-        <Script 
-          src="https://t.contentsquare.net/uxa/c3950e13d7005.js" 
-          strategy="afterInteractive" 
+        <Script
+          src="https://t.contentsquare.net/uxa/c3950e13d7005.js"
+          strategy="afterInteractive"
         />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '989212627079878');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=989212627079878&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className={`${inter.variable} ${plusJakartaSans.variable} font-sans antialiased min-h-screen bg-slate-50 selection:bg-brand-primary/20 selection:text-brand-secondary`}>
         <Tracking />
